625f1dcb9bbd9f325a942d64e7a5aab08ee9bee2
ankur-funongo <ankur.doshi@funongo.com>
2019-04-26 19:24:50 +0530

    Few additions

---
+++ app/src/main/java/com/kotlin/downloadablefonts/Constants.kt
@@ -0,0 +1,18 @@
+package com.kotlin.downloadablefonts
+
+/**
+ * Created by Ankur on 4/26/2019.
+ */
+//internal object Constants {
+    const val WIDTH_DEFAULT = 100
+    const val WIDTH_MAX = 1000
+    const val WIDTH_MIN = 0
+
+    const val WEIGHT_DEFAULT = 400
+    const val WEIGHT_MAX = 1000
+    const val WEIGHT_MIN = 0
+
+    const val ITALIC_DEFAULT = 0f
+    const val ITALIC_MAX = 1f
+    const val ITALIC_MIN = 0f
+//}
--- app/src/main/java/com/kotlin/downloadablefonts/MainActivity.kt
+++ app/src/main/java/com/kotlin/downloadablefonts/MainActivity.kt
@@ -4,10 +4,19 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
+import android.util.Log
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    private const val TAG = MainActivity::class.simpleName
+
     private lateinit var mHandler: Handler
+
+    lateinit private var mWidthSeekBar: SeekBar
+    lateinit private var mWeightSeekBar: SeekBar
+    lateinit private var mItalicSeekBar: SeekBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +29,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initializeSeekBars() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        mWidthSeekBar = findViewById<SeekBar>(R.id.seek_bar_width)
+        val widthValue = (100 * WIDTH_DEFAULT / WIDTH_MAX)
+        Log.d(TAG, "width is $widthValue")
+        mWidthSeekBar.progress = widthValue
+
+        val widthTextView = findViewById<TextView>(R.id.textview_width)
+        widthTextView.text = widthValue.toString()
+
+        mWidthSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                widthTextView.text = progressToWidth(progress).toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+    }
+
+    private fun progressToWidth(progress: Int) {
+
     }
 }
